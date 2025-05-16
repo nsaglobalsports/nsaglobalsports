a7ff1d01c2d2211be3e81ece40970bdf5a0849db
nsaglobalsports <abcba0191@gmail.com>
2025-05-16 17:37:19 +0530

    website upload

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', event => {
+  console.log('Service Worker installing...');
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated!');
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return new Response('Offline mode active.');
+    })
+  );
+});
